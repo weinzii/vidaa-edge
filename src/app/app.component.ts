2adc98ef5367c12999284ef1564de96c566776b6
weinzii <81271573+weinzii@users.noreply.github.com>
2025-01-15 22:45:26 +0100

    fix #2 mobile responsive
    https://github.com/weinzii/vidaa-edge/issues/2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ConsoleModalComponent } from "./components/console-modal/console-modal.component";
+import { NgIf } from '@angular/common';
 
 @Component({
-  imports: [RouterModule, ConsoleModalComponent, ConsoleModalComponent],
+  imports: [RouterModule, ConsoleModalComponent, ConsoleModalComponent, NgIf],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   @ViewChild(ConsoleModalComponent) consoleModalComponent!: ConsoleModalComponent;
+  isMobileMenuOpen = false;
 
   constructor() { }
 
   openConsoleModal(): void {
     this.consoleModalComponent.openModal();
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
   }
 }
